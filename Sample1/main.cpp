7c06c805d074d50ad64db6dd5c95d5a2d3dc8bf5
korfriend <korfriend@gmail.com>
2020-09-04 23:14:45 +0900

    fix source control merging problems

--- Sample1/main.cpp
+++ Sample1/main.cpp
@@ -131,11 +131,7 @@ int main()
 		{"gaussian_kernel_width", 5}, {"simplify_grid_length", 1.f}, {"geometric_complexity_kernel_ratio", 0.01f} });
 
 	// w/ interactive point set rendering
-<<<<<<< HEAD
 	float g_h = g_h_init; // control parameter (ct value)
-=======
-	float g_h = 5000.f; // control parameter (ct value)
->>>>>>> d5179c6e148847296050ad00246f81df2c9d5e49
 	float mu_u = 0.3f; // control parameter (0.0 ~ 1.0)
 	{
 		vzm::ExecuteModule2("ct_modeler.dll", "function_launcher", { candidate_pts_id },
@@ -235,67 +231,6 @@ int main()
 		}
 		key = cv::waitKey(1);
 	}
-
-	std::map<std::string, int> prim_ids = { {"candidate_pts_id", candidate_pts_id} , {"reliable_pts_id", reliable_pts_id} , {"holefilling_pts_id", holefilling_pts_id} , 
-	{"ls_reliable_pts_id", ls_reliable_pts_id} , {"ls_holefilling_pts_id", ls_holefilling_pts_id} , {"final_mesh_id", final_mesh_id} };
-
-	// test rendering example
-#define __cv3__ *(glm::fvec3*)
-#define __cv4__ *(glm::fvec4*)
-#define __cm4__ *(glm::fmat4x4*)
-	vzm::CameraParameters cam_params;
-	__cv3__ cam_params.pos = glm::fvec3(0, 0, 150);
-	__cv3__ cam_params.up = glm::fvec3(0, 1.f, 0);
-	__cv3__ cam_params.view = glm::fvec3(0, 0, -1.f);
-	cam_params.fov_y = 3.141592654f / 4.f;
-	cam_params.aspect_ratio = 640.f / 480.f;
-	cam_params.projection_mode = 2;
-	cam_params.w = 640;
-	cam_params.h = 480;
-
-	vzm::SceneEnvParameters scn_env_params;
-	scn_env_params.is_on_camera = true;
-	scn_env_params.is_pointlight = true;
-	__cv3__ scn_env_params.pos_light = __cv3__ cam_params.pos;
-	__cv3__ scn_env_params.dir_light = __cv3__ cam_params.view;
-
-	vzm::ObjStates obj_state;
-	obj_state.emission = 0.4f;
-	obj_state.diffusion = 0.6f;
-	obj_state.specular = 0.2f;
-	obj_state.sp_pow = 30.f;
-	__cv4__ obj_state.color = glm::fvec4(0.8f, 1.f, 1.f, 0.7f);
-	__cm4__ obj_state.os2ws = glm::fmat4x4(); // identity
-	
-	int scene_id = 0;
-	auto show_window = [](const std::string& title, const int scene_id, const int cam_id)
-	{
-		static uint count = 0;
-		static double times_sum = 0;
-		vzm::RenderScene(scene_id, cam_id);
-		std::cout << "rendering sec : " << times_sum / (double)(++count) << " s" << std::endl;
-		unsigned char* ptr_rgba;
-		float* ptr_zdepth;
-		int w, h;
-		if (vzm::GetRenderBufferPtrs(scene_id, &ptr_rgba, &ptr_zdepth, &w, &h, cam_id))
-		{
-			cv::Mat cvmat(h, w, CV_8UC4, ptr_rgba);
-			//show the image
-			cv::imshow(title, cvmat);
-		}
-	};
-	for (auto& it : prim_ids)
-	{
-		vzm::SetSceneEnvParameters(scene_id, scn_env_params);
-		vzm::SetCameraParameters(scene_id, cam_params, 0);
-		vzm::ReplaceOrAddSceneObject(scene_id, it.second, obj_state);
-
-		cv::namedWindow(it.first, 1);
-		show_window(it.first, scene_id, 0);
-		scene_id++;
-	}
-
-	cv::waitKey(0);
 
 	vzm::DeinitEngineLib();
 	return 0;
